9040a8cb47ac30e6466edf2fa153b784c305c225
safwen-code <safwendjebbi1234@gmail.com>
2021-10-03 23:37:19 +0100

    app est mis en jeu v1.0.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import Banner from "./Components/Banner";
+import Cart from "./Components/Cart";
+import ShoppingList from "./Components/ShoppingList";
+import QuestionForm from "./Components/QuestionForm";
 function App() {
-  return (<Banner />);
+  return (
+    <>
+      <Banner />
+      <Cart />
+      <ShoppingList />
+      <QuestionForm />
+    </>
+  );
 }
 
 export default App;
