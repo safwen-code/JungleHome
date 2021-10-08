ec7c4a3d58b23d4584cada88e39a53f5457b5786
safwen-code <safwendjebbi1234@gmail.com>
2021-10-08 16:13:33 +0100

    use localStoreg to save my Cards in browser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState, useEffect } from "react";
 import Banner from "./Components/Banner";
 import Cart from "./Components/Cart";
 import ShoppingList from "./Components/ShoppingList";
-import  logo1  from "./assets/logo1.png";
+import logo1 from "./assets/logo1.png";
 import Footer from "./Components/Footer";
 import "./Styles/Layout.css";
+
 function App() {
+  const savedCart = localStorage.getItem("cart");
+  const [cart, setcart] = useState(savedCart ? JSON.parse(savedCart) : []);
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
   return (
     <div>
       <Banner>
@@ -12,8 +19,8 @@ function App() {
         <h1 className="lmj-title">La maison jungle</h1>
       </Banner>
       <div className="lmj-layout-inner">
-        <Cart />
-        <ShoppingList />
+        <Cart cart={cart} setcart={setcart} />
+        <ShoppingList cart={cart} setcart={setcart} />
       </div>
       <Footer />
     </div>
