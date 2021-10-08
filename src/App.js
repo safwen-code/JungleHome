ed31d0be2fc81a677948d3f34d9b51cf2c68ae16
safwen-code <safwendjebbi1234@gmail.com>
2021-10-08 15:27:03 +0100

    add more photos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Banner from "./Components/Banner";
 import Cart from "./Components/Cart";
 import ShoppingList from "./Components/ShoppingList";
-import QuestionForm from "./Components/QuestionForm";
+import  logo1  from "./assets/logo1.png";
+import Footer from "./Components/Footer";
+import "./Styles/Layout.css";
 function App() {
   return (
-    <>
-      <Banner />
-      <Cart />
-      <ShoppingList />
-      <QuestionForm />
-    </>
+    <div>
+      <Banner>
+        <img src={logo1} alt="La maison jungle" className="lmj-logo" />
+        <h1 className="lmj-title">La maison jungle</h1>
+      </Banner>
+      <div className="lmj-layout-inner">
+        <Cart />
+        <ShoppingList />
+      </div>
+      <Footer />
+    </div>
   );
 }
 
