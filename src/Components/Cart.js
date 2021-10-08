6d100db3d5265a10ee92a9997600a949745bd73c
safwen-code <safwendjebbi1234@gmail.com>
2021-10-08 13:49:52 +0100

    create multipul function to update my cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,19 +1,29 @@
 import "../Styles/Cart.css";
+import { useState } from "react";
 
 function Cart() {
   const monsteraPrice = 8;
-  const ivyPrice = 10;
-  const flowerPrice = 15;
-  return (
+  const [cart, setcart] = useState(0);
+  const [isOpen, setisOpen] = useState(false);
+  function UpdateCart() {
+    setcart(cart + 1);
+    console.log("updatedCart");
+  }
+  function viderPanier() {
+    setcart(0);
+  }
+  return isOpen ? (
     <div className="lmj-cart">
+      <button onClick={() => setisOpen(false)}>Fermer</button>
       <h2>Panier</h2>
-      <ul>
-        <li>Monstera : {monsteraPrice}€</li>
-        <li>Lierre : {ivyPrice}€</li>
-        <li>Fleurs : {flowerPrice}€</li>
-      </ul>
-      Total : {monsteraPrice + ivyPrice + flowerPrice}€
+      <div>
+        Monstera : {monsteraPrice}€<button onClick={UpdateCart}>Ajouter</button>
+      </div>
+      <h3>Total : {monsteraPrice * cart}€</h3>
+      <button onClick={viderPanier}>vider le panier</button>
     </div>
+  ) : (
+    <button onClick={() => setisOpen(true)}>Ouvrir le Panier</button>
   );
 }
 
